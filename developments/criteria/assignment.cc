e2ed92672e652b2c72086f9e560016234a6a528c
sdgrecco <sdgrecco@student.cs.uwaterloo.ca>
2019-11-28 20:53:45 -0500

    merge conflicts fixed

--- developments/criteria/assignment.cc
+++ developments/criteria/assignment.cc
@@ -1,13 +1,18 @@
 #include "assignment.h"
 
-Assignment::Assignment(int locationVal):
+Assignment::Assignment(int locationVal): 
     Criterion{locationVal}{}
 
-void Assignment::complete(Player *player) {}
+
+void Assignment::complete(Player* player) {
+    for(int i = 0; i < 6; ++i){
+    
+    }
+}
 
 void Assignment::improve() {}
 
-//void Assignment::distributeResources() {}
+void Assignment::distributeResources() {}
 
 int Assignment::getCriterionType(){
     return 1;
