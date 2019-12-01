6e3cb8da4963b4d24b68386f6fda62a53a116984
dscostiu <dscostiu@student.cs.uwaterloo.ca>
2019-11-30 21:24:49 -0500

    update achieve to match complete; make complete override

--- developments/goal/achievement.cc
+++ developments/goal/achievement.cc
@@ -6,25 +6,17 @@ Achievement::Achievement(int locationVal, std::vector<int> cost):
     Goal{locationVal, cost} {}
 
 void Achievement::achieve(Player *owner){
-    ////////////////////
-    // NEED TO CHECK IF ADJACENT CRITERIA/GOALS SET; WHO WILL DO THAT, WHERE AND HOW??
-    ////////////////////
 
-    std::cout << "entered achieve fnc" << std::endl;
-    if (isSet()){
-        std::cout << "ENTERED isSet() FUNCTION" << std::endl;
-        throw "AlreadyAchievedException"; // TODO Implement actual Exception
-    }
-
-    if (false){ // TODO do actual check on player ("!playerHasEnoughResources: payment != cost")
-        throw "InsufficientResourcesException"; // TODO Implement actual Exception
+    if(!isSet() /*AND IF THERE IS NO ADJACENT*/){
+        
+        if(player->purchase(getCost(), this)) {
+            setDevelopment(owner);
+            //alert text display of new owner/criteria type somehow
+        } else {
+            throw "InsufficientResourcesException";
         }
-    else {
-        setDevelopment(owner);
-        //owner->addGoal(this); // TODO Refactor this using Visitor Pattern
+    } else {
+        throw "AlreadyAchievedException";
     }
 
-    ///////////////////////////////////////////////////////////////////////////
-    // SOMEBODY NEEDS TO NOTIFY TEXTDISPLAY TO UPDATE ITS DISPLAY FOR THAT GOAL
-    ///////////////////////////////////////////////////////////////////////////
 }
