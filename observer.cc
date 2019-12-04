7b0aa6a62edb2dbde26c357a3ee78f4ed62cabe6
sdgrecco <sdgrecco@student.cs.uwaterloo.ca>
2019-12-03 19:26:24 -0500

    merging

--- observer.cc
+++ observer.cc
@@ -1,16 +1,10 @@
 #include "observer.h"
 #include <string>
 
-class Resource;
+//class Resource;
 
-class Observer {
-    public:
-        virtual void notify() = 0;
-        virtual void notify(Resource) = 0;
-        virtual void notify(int) = 0;
-        virtual void notify(int, char, std::string) = 0;
-        virtual ~Observer() = 0;
-};
+Observer::~Observer() {}
+
 
 
 
--- subject.h
+++ subject.h
@@ -8,9 +8,9 @@ class Observer;
 class Subject {
     std::vector<Observer*> observers;
     public:
-        virtual void attach(Observer*) = 0;
-        virtual void detach(Observer*) = 0;
-        virtual void notifyObservers() = 0;
+        //virtual void attach(Observer*) = 0;
+        //virtual void detach(Observer*) = 0;
+        //virtual void notifyObservers() = 0;
         virtual ~Subject();
 };
 
